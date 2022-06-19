27a03c7b13206be9b53fd23ec1e204de5c06ccbf
ajita0303 <84762177+ajita0303@users.noreply.github.com>
2022-06-19 11:39:15 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector("#menu-btn");
+let header = document.querySelector(".header");
+
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  header.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  header.classList.remove("active");
+};
+
+let themeToggler = document.querySelector("#theme-toggler");
+
+themeToggler.onclick = () => {
+  themeToggler.classList.toggle("fa-sun");
+  if (themeToggler.classList.contains("fa-sun")) {
+    document.body.classList.add("active");
+  } else {
+    document.body.classList.remove("active");
+  }
+};
+
+new TypeIt("#username", {
+  waitUntilVisible: true,
+  speed: 190,
+  loop: true,
+  cursor: false,
+})
+  .type("Ajita Agrawal")
+  .exec(async () => {
+    await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        return resolve();
+      }, 2500);
+    });
+  })
+  .go();
